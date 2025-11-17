64c28616cf4192f7925ddb971852c893b0c0de0b
jtdbrab <jules.db.dev@gmail.com>
2025-11-17 21:46:55 +0100

    nothing too special

--- src/split_nodes.py
+++ src/split_nodes.py
@@ -45,3 +45,12 @@ def split_nodes_link(old_nodes):
         else:
             new_nodes.append(node)
     return new_nodes
+
+def text_to_textnodes(text):
+    nodes = [TextNode(text, TextType.NORMAL)]
+    nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+    nodes = split_nodes_delimiter(nodes, "*", TextType.ITALIC)
+    nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+    return nodes
