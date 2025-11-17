a1dc6a19e3f19ff0b7a7ae38f61c33849ee815d0
jtdbrab <jules.db.dev@gmail.com>
2025-11-17 21:43:38 +0100

    add splitting text into links and images

--- src/split_nodes.py
+++ src/split_nodes.py
@@ -1,4 +1,5 @@
 from textnode import TextNode, TextType
+import re
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
@@ -15,4 +16,32 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
             new_nodes.append(node)
     return new_nodes
 
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    pattern = r'!\[([^\]]+)\]\(([^)]+)\)'
+    for node in old_nodes:
+        if isinstance(node, TextNode) and node.text_type == TextType.NORMAL:
+            parts = re.split(pattern, node.text)
+            for i in range(0, len(parts), 3):
+                if parts[i]:
+                    new_nodes.append(TextNode(parts[i], TextType.NORMAL))
+                if i + 1 < len(parts):
+                    new_nodes.append(TextNode(parts[i + 1], TextType.IMAGE, parts[i + 2]))
+        else:
+            new_nodes.append(node)
+    return new_nodes
 
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    pattern = r'\[([^\]]+)\]\(([^)]+)\)'
+    for node in old_nodes:
+        if isinstance(node, TextNode) and node.text_type == TextType.NORMAL:
+            parts = re.split(pattern, node.text)
+            for i in range(0, len(parts), 3):
+                if parts[i]:
+                    new_nodes.append(TextNode(parts[i], TextType.NORMAL))
+                if i + 1 < len(parts):
+                    new_nodes.append(TextNode(parts[i + 1], TextType.LINK, parts[i + 2]))
+        else:
+            new_nodes.append(node)
+    return new_nodes
