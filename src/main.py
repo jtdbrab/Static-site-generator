2fd69fdbff57e5eeb6b856170642d8750cc3a112
jtdbrab <jules.db.dev@gmail.com>
2025-11-18 11:47:47 +0100

    done?

--- src/main.py
+++ src/main.py
@@ -1,12 +1,22 @@
 import os
 import shutil
+import sys
+from pathlib import Path
 from textnode import TextNode, TextType
 from markdown_utils import markdown_to_html_node
 
-def copy_directory(src, dst):
-    if os.path.exists(dst):
-        shutil.rmtree(dst)
-    shutil.copytree(src, dst)
+def copy_files_recursive(source_dir_path, dest_dir_path):
+    if not os.path.exists(dest_dir_path):
+        os.mkdir(dest_dir_path)
+
+    for filename in os.listdir(source_dir_path):
+        from_path = os.path.join(source_dir_path, filename)
+        dest_path = os.path.join(dest_dir_path, filename)
+        print(f" * {from_path} -> {dest_path}")
+        if os.path.isfile(from_path):
+            shutil.copy(from_path, dest_path)
+        else:
+            copy_files_recursive(from_path, dest_path)
 
 def extract_title(markdown):
     for line in markdown.split('\n'):
@@ -14,42 +24,56 @@ def extract_title(markdown):
             return line[2:].strip()
     raise Exception("No H1 title found in markdown")
 
-def generate_page(from_path, template_path, dest_path):
-    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
-    
-    with open(from_path, 'r') as f:
-        markdown_content = f.read()
-    
-    with open(template_path, 'r') as f:
-        template_content = f.read()
-    
-    html_content = markdown_to_html_node(markdown_content).to_html()
-    title = extract_title(markdown_content)
-    
-    full_html = template_content.replace('{{ Title }}', title).replace('{{ Content }}', html_content)
-    
-    os.makedirs(os.path.dirname(dest_path), exist_ok=True)
-    with open(dest_path, 'w') as f:
-        f.write(full_html)
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
+    for filename in os.listdir(dir_path_content):
+        from_path = os.path.join(dir_path_content, filename)
+        dest_path = os.path.join(dest_dir_path, filename)
+        if os.path.isfile(from_path):
+            dest_path = Path(dest_path).with_suffix(".html")
+            generate_page(from_path, template_path, dest_path, basepath)
+        else:
+            generate_pages_recursive(from_path, template_path, dest_path, basepath)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
-    for root, _, files in os.walk(dir_path_content):
-        for file in files:
-            if file.endswith('.md'):
-                from_path = os.path.join(root, file)
-                relative_path = os.path.relpath(from_path, dir_path_content)
-                dest_path = os.path.join(dest_dir_path, os.path.splitext(relative_path)[0] + '.html')
-                generate_page(from_path, template_path, dest_path)
+
+def generate_page(from_path, template_path, dest_path, basepath):
+    print(f" * {from_path} {template_path} -> {dest_path}")
+    from_file = open(from_path, "r")
+    markdown_content = from_file.read()
+    from_file.close()
+
+    template_file = open(template_path, "r")
+    template = template_file.read()
+    template_file.close()
+
+    node = markdown_to_html_node(markdown_content)
+    html = node.to_html()
+
+    title = extract_title(markdown_content)
+    template = template.replace("{{ Title }}", title)
+    template = template.replace("{{ Content }}", html)
+    template = template.replace('href="/', 'href="' + basepath)
+    template = template.replace('src="/', 'src="' + basepath)
+
+dir_path_static = "./static"
+dir_path_public = "./docs"
+dir_path_content = "./content"
+template_path = "./template.html"
+default_basepath = "/"
 
 def main():
-    # Delete anything in the 'public' directory
-    copy_directory('static', 'public')
-    
-    # Generate pages for every markdown file in the 'content' directory
-    generate_pages_recursive('content', 'template.html', 'public')
-    
-    node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
-    print(node)
+    basepath = default_basepath
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+
+    print("Deleting public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
+
+    print("Copying static files to public directory...")
+    copy_files_recursive(dir_path_static, dir_path_public)
+
+    print("Generating content...")
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public, basepath)
 
 if __name__ == "__main__":
     main()
